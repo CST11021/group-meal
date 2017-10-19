7042219e8728ef2453f540fc6b25283e7e33804e
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2017-10-19 11:54:53 +0800

    菜品后台管理

---
+++ group-meal-start/src/test/java/com/group/meal/dao/mapper/GroupItemCategoryDaoTest.java
@@ -0,0 +1,49 @@
+package com.group.meal.dao.mapper;
+
+import com.group.meal.ApplicationTests;
+import com.group.meal.dao.dataobject.GroupItemCategoryDO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GroupItemCategoryDaoTest extends ApplicationTests {
+
+    @Autowired
+    private GroupItemCategoryDao itemCategoryDao;
+
+    @Test
+    public void deleteByPrimaryKey() throws Exception {
+        int count = itemCategoryDao.deleteByPrimaryKey(1L);
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void insert() throws Exception {
+        GroupItemCategoryDO categoryDO = new GroupItemCategoryDO();
+        categoryDO.setCategoryName("早餐");
+        categoryDO.setParentId(0L);
+        categoryDO.setIsDel((byte) 0);
+        categoryDO.setStatus((byte) 1);
+        int count = itemCategoryDao.insert(categoryDO);
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void selectByPrimaryKey() throws Exception {
+        GroupItemCategoryDO categoryDO = itemCategoryDao.selectByPrimaryKey(1L);
+        Assert.assertNotEquals(null, categoryDO);
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() throws Exception {
+        GroupItemCategoryDO categoryDO = new GroupItemCategoryDO();
+        categoryDO.setId(1L);
+        categoryDO.setCategoryName("晚餐");
+        categoryDO.setParentId(1L);
+        categoryDO.setIsDel((byte) 1);
+        categoryDO.setStatus((byte) 0);
+        int count = itemCategoryDao.updateByPrimaryKeySelective(categoryDO);
+        Assert.assertEquals(1, count);
+    }
+
+}
