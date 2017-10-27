ea1a23ec15be0872dc546eed932bf600de04a1c6
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2017-10-27 09:44:14 +0800

    ItemSaveVO添加toString()方法

--- group-meal-controller/src/main/java/com/group/meal/vo/item/ItemSaveVO.java
+++ group-meal-controller/src/main/java/com/group/meal/vo/item/ItemSaveVO.java
@@ -1,8 +1,16 @@
 package com.group.meal.vo.item;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * @author wb-whz291815
  * @create 2017/10/16 15:15
  */
 public class ItemSaveVO {
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 }
