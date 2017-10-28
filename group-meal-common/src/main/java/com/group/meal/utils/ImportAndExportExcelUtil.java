c4fe8780ea9f5b08cc1f63af8b786e819c8cefe8
wanghz <2321322661@qq.com>
2017-10-28 16:51:49 +0800

    导出工具类优化

--- group-meal-common/src/main/java/com/group/meal/utils/ImportAndExportExcelUtil.java
+++ group-meal-common/src/main/java/com/group/meal/utils/ImportAndExportExcelUtil.java
@@ -11,6 +11,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -18,7 +21,9 @@ import java.util.*;
 /**
  * Created by whz on 2017/9/3.
  */
-public class ImportAndExportExcelUtil {
+public class ImportAndExportExcelUtil<T> {
+
+    public static final int MAX_NUM_PER_SHEET = 50000;
 
     public static final String EXCEL_SUFFIX_XLS = ".xls";
 
@@ -28,6 +33,7 @@ public class ImportAndExportExcelUtil {
 
     /**
      * 在应用根目录下创建一个文件夹，用于保存Excel文件
+     *
      * @param request
      * @param folderName 要创建的文件夹名称
      */
@@ -42,41 +48,22 @@ public class ImportAndExportExcelUtil {
     }
 
     /**
-     * 创建Excel文件，数据导入Excel，然后保存到指定的服务器路
-     * @param headFields    表头信息
-     * @param records       要导出的数据
-     * @param sheetName     表示Excel页签
-     * @param filepath      表示文件路径（路径 + 文件名 + 文件后缀）
+     * 创建Excel文件，数据导入Excel，然后保存到指定的服务器路径
+     *
+     * @param headFields 表头信息
+     * @param records    要导出的数据
+     * @param sheetName  表示Excel页签
+     * @param filepath   表示文件路径（路径 + 文件名 + 文件后缀）
      * @return Excel文件是否创建成功
      */
-    public static boolean createExcelFile(List<String> headFields, List<Map<String, String>> records, String sheetName, String filepath) {
+    public static boolean createExcelFile(List<String> headFields, Map<String, Integer> setWidth,
+                                          List<Map<String, String>> records, String sheetName, String filepath) {
         boolean flag;
 
         try {
             OutputStream os = new FileOutputStream(filepath);
             XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.createSheet(sheetName);
-            XSSFRow row = sheet.createRow(0);
-
-            int cell = 0;
-            for (String columnName : headFields) {
-                row.createCell(cell).setCellValue(columnName);
-                sheet.setColumnWidth(cell, columnName.getBytes().length*256);// 仅适用中文
-                cell++;
-            }
-
-            int rw = 1;
-            for (Map<String, String> record : records) {
-                XSSFRow xRow = sheet.createRow(rw);
-                cell = 0;
-                for (String columnName : headFields) {
-                    xRow.createCell(cell).setCellValue(record.get(columnName));
-                    sheet.autoSizeColumn(cell);// 不适用中文
-                    cell++;
-                }
-                rw++;
-            }
-
+            createSheet(workbook, sheetName, headFields, setWidth, records);
             workbook.write(os);
             os.close();
             flag = true;
@@ -89,66 +76,136 @@ public class ImportAndExportExcelUtil {
 
     /**
      * 创建Excel文件，数据导入Excel，然后保存到指定的服务器路(支持多个页签数据)
+     *
      * @param sheetHeadFields
      * @param sheetRecords
      * @param sheetNames
      * @param filePath
      * @return
      */
-    public static boolean createExcelFile(List<List<String>> sheetHeadFields, List<List<Map<String,String>>> sheetRecords, List<String> sheetNames, String filePath) {
+    public static boolean createExcelFile(List<List<String>> sheetHeadFields,
+                                          List<Map<String, Integer>> setWidths,
+                                          List<List<Map<String, String>>> sheetRecords,
+                                          List<String> sheetNames, String filePath) {
         boolean flg;
         try {
             OutputStream os = new FileOutputStream(filePath);
-            XSSFWorkbook wb = new XSSFWorkbook();
+            XSSFWorkbook workbook = new XSSFWorkbook();
 
             int sheetNum = sheetNames.size();
-            for(int sheetIndex = 0; sheetIndex < sheetNum; sheetIndex++) {
-
-                XSSFSheet sheet = wb.createSheet(sheetNames.get(sheetIndex));
-                XSSFRow row = sheet.createRow(0);
-
-                int cell = 0;
-                for (String columnName : sheetHeadFields.get(sheetIndex)) {
-                    Cell tempCell = row.createCell(cell);
-                    tempCell.setCellType(CellType.STRING);
-                    tempCell.setCellValue(columnName);
-                    sheet.setColumnWidth(cell, columnName.getBytes().length*256);// 仅适用中文
-                    cell++;
-                }
-
-                int rw = 1;
-                if(sheetRecords != null) {
-                    for (Map<String, String> record : sheetRecords.get(sheetIndex)) {
-                        XSSFRow xRow = sheet.createRow(rw);
-                        cell = 0;
-                        for (String columnName : sheetHeadFields.get(sheetIndex)){
-                            Cell tempCell = xRow.createCell(cell);
-                            tempCell.setCellType(CellType.STRING);
-                            tempCell.setCellValue(record.get(columnName));
-                            sheet.autoSizeColumn(cell);// 不适用中文
-                            cell++;
-                        }
-                        rw++;
-                    }
-                }
-
+            for (int sheetIndex = 0; sheetIndex < sheetNum; sheetIndex++) {
+                Map<String, Integer> setWidth = setWidths == null ? null : setWidths.get(sheetIndex);
+                createSheet(workbook, sheetNames.get(sheetIndex), sheetHeadFields.get(sheetIndex),
+                        setWidth, sheetRecords.get(sheetIndex));
             }
 
-            wb.write(os);
+            workbook.write(os);
             os.close();
             flg = true;
         } catch (Exception e) {
             flg = false;
+            e.printStackTrace();
         }
         return flg;
     }
 
     /**
+     * 导出Excel文件，直接将records数据写入客户端，效率相对较高
+     * @param headFields    导出列表的表头
+     * @param setWidth      设置列宽
+     * @param records       要导出的数据
+     * @param sheetName     页签名称
+     * @param exportFileName    导出的文件名称（如：统计.xlsx）
+     * @param response
+     * @return
+     */
+    public static boolean exportExcelFile(List<String> headFields,
+                                          Map<String, Integer> setWidth, List<Map<String, String>> records,
+                                          String sheetName, String exportFileName, HttpServletResponse response) {
+        boolean flag;
+
+        try {
+            OutputStream os = response.getOutputStream();
+            response.reset();
+            response.setHeader("Content-disposition", "attachment; filename="
+                    + URLEncoder.encode(exportFileName, "UTF-8"));
+            response.setContentType("application/msexcel");
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            createSheet(workbook, sheetName, headFields, setWidth, records);
+            workbook.write(os);
+            os.close();
+            flag = true;
+        } catch (Exception e) {
+            flag = false;
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    /**
+     * 导出多个页签的excel表,解决xls格式行数65535的限制
+     * @param headFields
+     * @param records
+     * @param sheetName
+     * @param response
+     * @throws Exception
+     */
+    public void exportExcelFile(LinkedHashMap<String, String> headFields, Map<String, Integer> setWidth,
+                        List<T> records, String sheetName, String exportFileName, HttpServletResponse response)
+            throws Exception {
+        OutputStream os = response.getOutputStream();
+        response.reset();
+        response.setHeader("Content-disposition", "attachment; filename="
+                + URLEncoder.encode(exportFileName, "UTF-8"));
+        response.setContentType("application/msexcel");
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        Map<Integer, List<T>> sheetMap = pagination(records);
+        Set<Integer> keys = sheetMap.keySet();
+
+        for(Integer SheetKey : keys){
+            HSSFSheet sheet = workbook.createSheet(sheetName + SheetKey);
+
+            HSSFRow row = sheet.createRow(0);
+            int cell = 0;
+            for (String columnName : headFields.keySet()) {
+                row.createCell(cell).setCellValue(columnName);
+                if (setWidth != null) {
+                    Integer width = setWidth.get(columnName);
+                    if (width == null || width.equals(0)) {
+                        int length = columnName.getBytes().length * 256;
+                        // 仅适用中文
+                        sheet.setColumnWidth(cell, length);
+                        // 不适用中文
+                        //sheet.autoSizeColumn(cell);
+                    } else {
+                        sheet.setColumnWidth(cell, width * 2 * 256);
+                    }
+                }
+                cell++;
+            }
+
+            List<T> sheetRows = sheetMap.get(SheetKey);
+            for (int i = 0, len = sheetRows.size(); i < len; i++) {
+                T en = sheetRows.get(i);
+                List<Object> dataList = reflectEntity(en, headFields.values());
+                row = sheet.createRow(i + 1);
+                for (int m = 0; m < dataList.size(); m++) {
+                    row.createCell(m).setCellValue(dataList.get(m).toString());
+                }
+            }
+        }
+        workbook.write(os);
+    }
+
+    /**
      * 将服务器上的文件下载到客户端（用户浏览器）
-     * @param path 服务器上的文件路径（路径 + 文件名 + 后缀）
+     *
+     * @param path     服务器上的文件路径（路径 + 文件名 + 后缀）
      * @param response
      */
-    public static void downloadFromService(String path, HttpServletResponse response) throws IOException{
+    public static void downloadFromService(String path, HttpServletResponse response) throws IOException {
         File file = new File(path);
         String fileName = file.getName();
         InputStream fis = new BufferedInputStream(new FileInputStream(path));
@@ -168,7 +225,107 @@ public class ImportAndExportExcelUtil {
     }
 
     /**
+     * 创建一个页签
+     *
+     * @param workbook
+     * @param sheetName
+     * @param headFields
+     * @param setWidth
+     * @param records
+     * @return
+     */
+    private static XSSFSheet createSheet(XSSFWorkbook workbook, String sheetName, List<String> headFields,
+                                         Map<String, Integer> setWidth, List<Map<String, String>> records) {
+
+        XSSFSheet sheet = workbook.createSheet(sheetName);
+        XSSFRow row = sheet.createRow(0);
+
+        int cell = 0;
+        for (String columnName : headFields) {
+            row.createCell(cell).setCellValue(columnName);
+            if (setWidth != null) {
+                Integer width = setWidth.get(columnName);
+                if (width == null || width.equals(0)) {
+                    // 仅适用中文
+                    sheet.setColumnWidth(cell, columnName.getBytes().length * 256);
+                    // 不适用中文
+                    //sheet.autoSizeColumn(cell);
+                } else {
+                    sheet.setColumnWidth(cell, width * 2 * 256);
+                }
+            }
+
+            cell++;
+        }
+
+        int rw = 1;
+        for (Map<String, String> record : records) {
+            XSSFRow xRow = sheet.createRow(rw);
+            cell = 0;
+            for (String columnName : headFields) {
+                xRow.createCell(cell).setCellValue(record.get(columnName));
+                cell++;
+            }
+            rw++;
+        }
+
+        return sheet;
+    }
+
+    /**
+     * 使用反射技术，返回一行Excel记录
+     * @param model
+     * @param <T>
+     * @return
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     * @throws SecurityException
+     * @throws NoSuchFieldException
+     */
+    private static <T> List<Object> reflectEntity(T model, Collection<String> fields)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        List<Object> list = new ArrayList<Object>();
+
+        for (String name : fields) {
+            name = name.substring(0, 1).toUpperCase() + name.substring(1);
+
+            Method m = model.getClass().getMethod("get" + name);
+            Object value = m.invoke(model);
+            list.add(value == null ? "" : value.toString());
+        }
+
+        return list;
+    }
+
+    /**
+     * 将大集合分成多个子集合
+     * @param allRecords
+     * @return
+     */
+    private Map<Integer, List<T>> pagination(List<T> allRecords) {
+        int totalPage = allRecords.size() / MAX_NUM_PER_SHEET;
+        int mod = allRecords.size() % MAX_NUM_PER_SHEET;
+        Map<Integer, List<T>> map = new HashMap();
+        for (int i = 0; i < totalPage; i++) {
+            List<T> subRecords;
+            if (i == totalPage) {
+                subRecords = allRecords.subList(i * MAX_NUM_PER_SHEET, MAX_NUM_PER_SHEET * i + mod);
+            } else {
+                subRecords = allRecords.subList(i * MAX_NUM_PER_SHEET, MAX_NUM_PER_SHEET * (i + 1));
+            }
+            map.put(i, subRecords);
+        }
+        return map;
+    }
+
+
+
+
+    /**
      * 获取第一个页签的第一行的表头信息
+     *
      * @param is
      * @return
      */
@@ -191,6 +348,7 @@ public class ImportAndExportExcelUtil {
 
     /**
      * 获取表格数据（从第二行开始，）
+     *
      * @param is
      * @return key：表示数据的索引；value：表示行数据，用“-”分割
      * @throws IOException
@@ -200,7 +358,8 @@ public class ImportAndExportExcelUtil {
         String str = "";
         Map<Integer, String> content = new HashMap<Integer, String>();
 
-        POIFSFileSystem poifsFileSystem = new POIFSFileSystem(is);;
+        POIFSFileSystem poifsFileSystem = new POIFSFileSystem(is);
+        ;
         Workbook workbook = new HSSFWorkbook(poifsFileSystem);
 
         Sheet sheet = workbook.getSheetAt(0);
@@ -264,6 +423,7 @@ public class ImportAndExportExcelUtil {
 
     /**
      * 根据HSSFCell类型设置数据
+     *
      * @param cell
      * @return
      */
@@ -334,7 +494,7 @@ public class ImportAndExportExcelUtil {
         record2.add(recordMap2);
         records.add(record2);
 
-        createExcelFile(headFields, records, titles, filePath);
+        createExcelFile(headFields, null, records, titles, filePath);
     }
 
     public void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -358,7 +518,7 @@ public class ImportAndExportExcelUtil {
         String filePath = folderPath + FILE_SEPARATOR + "人员信息" + EXCEL_SUFFIX_XLSX;
 
         // 创建Excel文件，数据导入Excel，然后保存到指定的服务器路径
-        boolean flag = createExcelFile(headFields, records, "sheet1", filePath);
+        boolean flag = createExcelFile(headFields, null, records, "sheet1", filePath);
         if (flag) {
             // 将服务器上的文件下载到客户端（用户浏览器）
             downloadFromService(filePath, response);
